c5ae52c2dd6add7cea793e4ef80703d70dbddafb
ani <125677472+anilev6@users.noreply.github.com>
2023-10-31 11:55:42 +0100

    upd

---
+++ easy_open_ai/__init__.py
@@ -0,0 +1,3 @@
+from .functions.picture import get_picture, aget_picture, get_n_pictures, aget_n_pictures
+from .functions.text import get_answer, aget_answer, get_answer_with_instruction, aget_answer_with_instruction
+from .functions.text import correct_grammar, acorrect_grammar, translate_text_to_Ukrainian, atranslate_text_to_Ukrainian
--- easy_open_ai/models/api_key.py
+++ easy_open_ai/models/api_key.py
@@ -2,6 +2,10 @@ import os
 
 from dotenv import load_dotenv
 
-load_dotenv()  # Load variables from .env file
+# Get the current working directory
+current_directory = os.getcwd()
+dotenv_path = os.path.join(current_directory, '.env')
+
+load_dotenv(dotenv_path)  # Load variables from .env file
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
